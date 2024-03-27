2ca680605c2ad8a61e3e164490a65e43df0e3240
ritvij_saxena <saxenaritvij@gmail.com>
2024-03-27 12:40:45 -0400

    initial commit

---
+++ src/test/java/com/sparctechlabs/atypical_backend/AtypicalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparctechlabs.atypical_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtypicalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
